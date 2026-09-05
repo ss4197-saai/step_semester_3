e645f15cf45af46735896e495c4dbbc4332b9c23
ss4197-saai <ss4197@srmist.edu.in>
2026-09-05 10:16:29 +0530

    Create irstNonRepeating.java

---
+++ irstNonRepeating.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class FirstNonRepeating {
+
+    static char findFirstNonRepeatingChar(String text) {
+
+        int[] frequency = new int[256];
+
+        // Count frequency
+        for (int i = 0; i < text.length(); i++) {
+            frequency[text.charAt(i)]++;
+        }
+
+        // Find first character with frequency 1
+        for (int i = 0; i < text.length(); i++) {
+
+            if (frequency[text.charAt(i)] == 1) {
+                return text.charAt(i);
+            }
+        }
+
+        return '\0';
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word or sentence: ");
+        String text = sc.nextLine();
+
+        char result = findFirstNonRepeatingChar(text);
+
+        if (result == '\0') {
+            System.out.println(
+                "No Non-Repeating Character Found"
+            );
+        } else {
+            System.out.println(
+                "First Non-Repeating Character: '" +
+                result + "'"
+            );
+        }
+
+        sc.close();
+    }
+}
