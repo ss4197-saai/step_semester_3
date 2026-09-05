33dc75e1975a42e12f4557b77f4abed34a8b4d1e
ss4197-saai <ss4197@srmist.edu.in>
2026-09-05 10:18:35 +0530

    Create ReverseCustomerName .java

---
+++ ReverseCustomerName .java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class ReverseCustomerName {
+
+    static String reverseCustomerName(String customerName) {
+
+        char[] characters = customerName.toCharArray();
+
+        String reversed = "";
+
+        for (int i = characters.length - 1; i >= 0; i--) {
+            reversed = reversed + characters[i];
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter customer name: ");
+        String customerName = sc.nextLine();
+
+        String reversedName =
+            reverseCustomerName(customerName);
+
+        System.out.println("\nOriginal Name: " + customerName);
+        System.out.println("Reversed Name: " + reversedName);
+
+        sc.close();
+    }
+}
